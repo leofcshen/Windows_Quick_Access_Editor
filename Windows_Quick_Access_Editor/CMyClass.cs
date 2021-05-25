c55e5426beef024f52508957293bf07b04028ff5
rovingwind-PC <rovingwindownload@gmail.com>
2021-05-26 05:24:06 +0800

    update function

---
+++ Windows_Quick_Access_Editor/CMyClass.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Quick_Access_Editor
+{
+    class CMyClass
+    {
+        private string _configPath = "\\config.ini";
+        public string configPath { get; } // config.ini path
+        public static Type shellAppType = Type.GetTypeFromProgID("Shell.Application");
+        public static Object shell = Activator.CreateInstance(shellAppType);
+        public Shell32.Folder2 f2 = (Shell32.Folder2)shellAppType.InvokeMember
+                ("NameSpace", System.Reflection.BindingFlags.InvokeMethod, null, shell, new object[] { "shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}" });
+        public string[] defaultQuickAccess = new string[4] { "桌面", "下載", "文件", "圖片" };
+        public class CDicValue
+        {
+            public string Name { get; set; }
+            public string Path { get; set; }
+        }
+        /// <summary>
+        /// 目前的快速存取清單
+        /// </summary>
+        public Dictionary<int, CDicValue> dCurrentList { get; set; }
+        public CMyClass()
+        {
+            mAddQuickAccessToDic();
+            configPath = Directory.GetCurrentDirectory() + _configPath;
+        }
+        
+        /// <summary>
+        /// 確認 config.ini 是否存在
+        /// </summary>
+        public bool mCheckConfigFile()
+        {            
+            if (File.Exists(configPath))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 新增 config.ini
+        /// </summary>
+        /// <returns>string 執行結果</returns>
+        public string mAddConfigFile() 
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(configPath, FileMode.Create, FileAccess.Write))
+                {
+                    using (StreamWriter sr = new StreamWriter(fs))
+                    {
+                        foreach (var item in dCurrentList)
+                            sr.WriteLine(item.Value.Path);
+                    }
+                }
+                return "config.ini 檔已建立，請編輯後使用。";
+            }
+            catch (Exception e) { return $"config.ini 建立失敗，錯誤碼 {e.Message}"; }            
+        }
+
+        /// <summary>
+        /// 刪除自訂快速存取
+        /// </summary>
+        /// <returns>string 執行結果</returns>
+        public string mDelCustomer()
+        {            
+            // 路徑在 "%AppData%\Microsoft\Windows\Recent\AutomaticDestinations\f01b4d95cf55d32a.automaticDestinations-ms"
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Microsoft\Windows\Recent\AutomaticDestinations\f01b4d95cf55d32a.automaticDestinations-ms";
+            try
+            {
+                File.Delete(filePath);
+                return "刪除自訂快速存取檔案完成。";
+            }
+            catch (Exception e) { return "刪除自訂快速存取失敗，錯誤碼：" + e.Message; }
+        }
+
+        /// <summary>
+        /// 刪除單筆快速存取
+        /// </summary>
+        /// <returns>string 執行結果</returns>
+        public string mDelSingle(string delFolder)
+        {
+            foreach (Shell32.FolderItem fi in f2.Items())
+            {
+                if (fi.Name == delFolder)
+                {
+                    ((Shell32.FolderItem)fi).InvokeVerb("unpinfromhome");
+                    break;
+                }
+            }
+            return "刪除單筆完成：" + delFolder;
+        }
+
+        /// <summary>
+        /// 把快速存取清單加到 dCurrentList
+        /// </summary>
+        public void mAddQuickAccessToDic()
+        {
+            f2 = (Shell32.Folder2)shellAppType.InvokeMember
+                ("NameSpace", System.Reflection.BindingFlags.InvokeMethod, null, shell, new object[] { "shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}" });
+            dCurrentList = new Dictionary<int, CDicValue>();
+            int count = 1;
+            foreach (Shell32.FolderItem fi in f2.Items())
+            {
+                CDicValue cDicValue = new CDicValue { Name = fi.Name, Path = fi.Path};
+                dCurrentList.Add(count, cDicValue);
+                count++;
+            }
+        }
+
+        /// <summary>
+        /// 新增快速存取
+        /// </summary>   
+        /// <returns>string 執行結果</returns>
+        public string mAddQuickAccess(List<string> lisFolder)
+        {
+            string str = string.Empty;
+
+            foreach (var item in lisFolder)
+            {
+                f2 = (Shell32.Folder2)shellAppType.InvokeMember
+                    ("NameSpace", System.Reflection.BindingFlags.InvokeMethod, null, shell, new object[] { item });
+                try
+                {
+                    f2.Self.InvokeVerb("pintohome");
+                    str += $"新增 {item} 成功。\n";
+                }
+                catch (Exception e) { str += $"新增 {item} 失敗，錯誤碼：{e.Message}。\n"; }
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// 讀取 config.ini 傳回 configList
+        /// </summary>
+        /// <returns>bool 成敗</returns>
+        public bool mReadConfig(out List<string> configList)
+        {
+            configList = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(configPath, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                        configList.Add(line);
+                }
+                return true;
+            }
+            catch (Exception ex) { return false; }
+        }
+        /// <summary>
+        /// 匯出快速存取到 config.ini
+        /// </summary>
+        /// <returns>string 執行結果</returns>
+        public string mExportConfig()
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(configPath, FileMode.Create, FileAccess.Write))
+                {
+                    using (StreamWriter sr = new StreamWriter(fs))
+                    {
+                        foreach (var item in dCurrentList)
+                            sr.WriteLine(item.Value.Path);
+                    }
+                }
+                return "config.ini 已匯出。";
+            }
+            catch (Exception e) { return $"config.ini 匯出失敗，錯誤碼：{e.Message}。"; }
+        }
+
+        /// <summary>
+        /// 刪除所有快速存取
+        /// </summary>
+        public string mDelAll()
+        {
+            foreach (Shell32.FolderItem fi in f2.Items())
+            {
+                ((Shell32.FolderItem)fi).InvokeVerb("unpinfromhome");
+            }
+            return "刪除所有快速存取已完成。";
+        }
+
+        /// <summary>
+        /// 新增預設快速存取
+        /// </summary>
+        public string mAddDefault()
+        {
+            List<string> lisFolder = new List<string>() {
+                @"C:\Users\rovin\Desktop",
+                @"C:\Users\rovin\Downloads",
+                @"C:\Users\rovin\Documents",
+                @"C:\Users\rovin\Pictures",
+            };
+            string str = string.Empty;
+
+            foreach (var item in lisFolder)
+            {
+                f2 = (Shell32.Folder2)shellAppType.InvokeMember
+                    ("NameSpace", System.Reflection.BindingFlags.InvokeMethod, null, shell, new object[] { item });
+                try
+                {
+                    f2.Self.InvokeVerb("pintohome");
+                    str += $"新增 {item} 成功。\n";
+                }
+                catch (Exception e) { str += $"新增 {item} 失敗，錯誤碼：{e.Message}。\n"; }
+            }
+            return str;
+        }
+    }
+}
--- Windows_Quick_Access_Editor/Program.cs
+++ Windows_Quick_Access_Editor/Program.cs
@@ -12,191 +12,220 @@ namespace Windows_Quick_Access_Editor
     {
         static void Main(string[] args)
         {
-            string configPath = Directory.GetCurrentDirectory() + "\\config.ini";
-            
-            if (!File.Exists(configPath)) // config.ini 如果不存在，建立檔案，寫入 default 路徑。
-            {
-                try
-                {
-                    using (FileStream fs = new FileStream(configPath, FileMode.Create, FileAccess.Write))
-                    {
-                        using (StreamWriter sr = new StreamWriter(fs))
-                        {
-                            sr.WriteLine("C:\\ProgramData");
-                            sr.WriteLine("C:\\Windows");
-                        }
-                    }
-                    Console.WriteLine("config.ini 檔已建立，請編輯後使用。");
-                }
-                catch (Exception e) { Console.WriteLine("config.ini 新增失敗，錯誤碼：" + e.Message); }
-            }
+            string strConfirm = "(確定：1、任意鍵取消)：";
+            CMyClass cMC = new CMyClass();
+
+            if (!cMC.mCheckConfigFile())
+                Console.WriteLine(cMC.mAddConfigFile());
             else
                 Console.WriteLine("config.ini 檔已存在，請編輯後使用。");
-            separator();
-
-            bool run = true; // 迴圈 flag
-            bool success; // 測試轉換選項
-            int number; // 選項
+            mNewLine();
+            mListConfig(cMC);
+            mNewLine();
+            mListCurrent(cMC);
+            mSeparator();
+                        
             do
             {
-                Console.WriteLine("1.刪除所有快速存取");
+                string defaultItem = string.Empty;
+                for (int i = 0; i < cMC.defaultQuickAccess.Length; i++)
+                {
+                    defaultItem += $"{cMC.defaultQuickAccess[i]}";
+                    if (i != cMC.defaultQuickAccess.Length - 1)
+                        defaultItem += "、";
+                }
+                Console.WriteLine($"[預設項目：{defaultItem}]");
+                mNewLine();
+                Console.WriteLine("1.刪除自訂快速存取(不含預設)");
                 Console.WriteLine("2.刪除單筆快速存取");
                 Console.WriteLine("3.新增批次快速存取");
-                Console.WriteLine("4.新增單筆快速存取");                
-                Console.WriteLine();
+                Console.WriteLine("4.新增單筆快速存取");
+                Console.WriteLine("5.匯出所有快速存取");
+                Console.WriteLine("6.刪除所有快速存取");
+                Console.WriteLine("7.新增預設快速存取");
+                mNewLine();
                 Console.Write("請選擇功能，輸入 0 離開：");
 
-                success = Int32.TryParse(Console.ReadLine(), out number); // 轉換選項
-                Console.WriteLine();
+                bool success = Int32.TryParse(Console.ReadLine(), out int option);
+                mNewLine();
 
                 if (success)
                 {
-                    switch (number)
+                    switch (option)
                     {
-                        case 1: // 刪除所有快速存取
-                            // 路徑在 "%AppData%\Microsoft\Windows\Recent\AutomaticDestinations\f01b4d95cf55d32a.automaticDestinations-ms"
-                            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Microsoft\Windows\Recent\AutomaticDestinations\f01b4d95cf55d32a.automaticDestinations-ms";
-                            if (File.Exists(filePath)) // 檔案存在的話刪除
+                        case 1: // 刪除自訂快速存取
+                            Console.Write("確認刪除自訂快速存取？" + strConfirm);
+                            success = Int32.TryParse(Console.ReadLine(), out option);
+                            mNewLine();
+                            if (success && option == 1)
                             {
-                                try
-                                {
-                                    File.Delete(filePath);
-                                    Console.WriteLine("刪除全部完成");
-                                }
-                                catch (Exception e) { Console.WriteLine("刪除全部失敗，錯誤碼：" + e.Message); }
+                                Console.WriteLine(cMC.mDelCustomer());
+                                mNewLine();
+                                mListCurrent(cMC);
                             }
                             else
-                                Console.WriteLine("檔案不存在");
-                            break;
-                        case 2: // 單筆移除快速存取
-                            delQuickAccess();                            
+                                mCancel();
                             break;
 
-                        case 3: // 批次新增快速存取                            
-                            List<string> addList = new List<string>(); // 用來存放 config.ini 的路徑
-
-                            try
+                        case 2: // 刪除單筆快速存取
+                            do
                             {
-                                if (File.Exists(configPath)) // config.ini 存在，把路徑存進 list。
-                                {                                    
-                                    using (StreamReader sr = new StreamReader(configPath, System.Text.Encoding.Default))
+                                mListCurrent(cMC);                                
+                                mNewLine();
+
+                                Console.Write("請選擇要移除的項目，輸入 0 離開：");
+                                success = Int32.TryParse(Console.ReadLine(), out option);
+                                mNewLine();
+
+                                if (success)
+                                {
+                                    if (option == 0) // 離開
+                                        break;
+                                    if (cMC.dCurrentList.ContainsKey(option)) // 用 key 尋找 Dictionary，找到的話刪除。
                                     {
-                                        string line;
-                                        while ((line = sr.ReadLine()) != null)
+                                        string delFolder = cMC.dCurrentList[option].Name;
+                                        if (option > 0 && option < 5) // 1-4 為系統預設
                                         {
-                                            addList.Add(line);
-                                            Console.WriteLine(line);
+                                            Console.Write("確認移除系統預設項目？" + strConfirm);
+                                            success = Int32.TryParse(Console.ReadLine(), out option);
+                                            mNewLine();
+                                            if (success && option == 1)
+                                                Console.WriteLine(cMC.mDelSingle(delFolder));
+                                            else 
+                                                mCancel();                                            
                                         }
                                     }
-                                    Console.WriteLine();
-                                    Console.Write("確認新增上列快速存取資料夾？(確定：1，取消：0)：");
-                                    success = Int32.TryParse(Console.ReadLine(), out number); // 轉換選項
-                                    if (success && number == 1)
+                                    else
+                                        Console.WriteLine("找不到項目：" + option);
+                                }
+                                mNewLine();
+                            } while (true);
+                            break;
+
+                        case 3: // 新增自訂快速存取                           
+                            try
+                            {
+                                if (cMC.mCheckConfigFile())
+                                {
+                                    if (cMC.mReadConfig(out List<string> addList))
                                     {
+                                        Console.WriteLine("[config.ini]");
                                         foreach (var item in addList)
-                                            addQuickAccess(item);
-                                        Console.WriteLine("新增批次成功");
+                                            Console.WriteLine(item);
                                     }
+                                    else Console.WriteLine("config.ini 讀取失敗。");
+                                    mNewLine();
+
+                                    Console.Write("確認新增上列快速存取資料夾？" + strConfirm);
+                                    success = Int32.TryParse(Console.ReadLine(), out option);
+                                    mNewLine();
+                                    if (success && option == 1)
+                                        Console.WriteLine(cMC.mAddQuickAccess(addList));
+                                    else
+                                        mCancel();
                                 }
                                 else
                                     Console.WriteLine("config.ini 不存在。");
+                                mListCurrent(cMC);
                             }
                             catch (Exception e) { Console.WriteLine("新增批次失敗，錯誤碼：" + e.Message); }
                             break;
 
-                        case 4: // 單筆新增快速存取
-                            Console.WriteLine("請輸入要加入快速存取的資料夾路徑：(例如 'D:\\PC')");
-                            string addFolderPath = Console.ReadLine();
-                            Console.WriteLine();
-                            addQuickAccess(addFolderPath);
+                        case 4: // 新增單筆快速存取
+                            Console.WriteLine("請輸入要加入快速存取的資料夾路徑：(例如 'D:\\PC')");                            
+                            List<string> lisFolder = new List<string> { Console.ReadLine() };
+                            mNewLine();
+                            Console.WriteLine(cMC.mAddQuickAccess(lisFolder));
+                            mListCurrent(cMC);
+                            break;
+
+                        case 5: // 匯出目前快速存取到 config.ini
+                            #region 列出目前的快速存取清單
+                            cMC.mAddQuickAccessToDic();
+                            foreach (var item in cMC.dCurrentList)
+                                Console.WriteLine(item.Key + "." + item.Value.Name);
+                            mNewLine();
+                            #endregion
+
+                            Console.Write("確認匯出到 config.ini？" + strConfirm);
+                            success = Int32.TryParse(Console.ReadLine(), out option);
+                            mNewLine();
+                            if (success && option == 1)
+                            {
+                                Console.WriteLine(cMC.mExportConfig());
+                                mNewLine();
+                                mListConfig(cMC);
+                            }
+                            else
+                                mCancel();
+                            break;
+
+                        case 6: // 刪除所有快速存取
+                            Console.Write("確認刪除所有快速存取？" + strConfirm);
+                            success = Int32.TryParse(Console.ReadLine(), out option);
+                            mNewLine();
+                            if (success && option == 1)
+                            {
+                                Console.WriteLine(cMC.mDelAll());
+                                mNewLine();
+                                mListCurrent(cMC);
+                            }
+                            else
+                                mCancel();
+                            break;
+
+                        case 7: // 新增預設快速存取
+                            Console.WriteLine(cMC.mAddDefault());
+                            Console.WriteLine("新增預設快速存取成功");
+                            mNewLine();
+                            mListCurrent(cMC);
                             break;
 
                         case 0: // 離開
-                            run = false;
-                            break;
+                            return;
                     }
                 }
-                separator();
-            } while (run);
-
-            Console.WriteLine("運行結束，按任意鍵繼續");
-            Console.Read();
+                mSeparator();
+            } while (true);
         }
 
-        static void separator() // 分隔線
+        /// <summary>
+        /// 列出目前的快速存取清單
+        /// </summary>
+        static void mListCurrent(CMyClass cMC)
+        {
+            cMC.mAddQuickAccessToDic();
+            Console.WriteLine("[目前的快速存取清單]");
+            foreach (var item in cMC.dCurrentList)
+                Console.WriteLine(item.Key + "." + item.Value.Name);
+        }
+
+        /// <summary>
+        /// 列出 config.ini
+        /// </summary>
+        static void mListConfig(CMyClass cMC)
+        {
+            Console.WriteLine("[config.ini]");
+
+            cMC.mReadConfig(out List<string> configList);
+            foreach (var item in configList)
+                Console.WriteLine(item);
+        }
+
+        static void mSeparator() // 分隔線
         {
             string str = new string('=', 50);
             Console.WriteLine(str);
         }
 
-        static void addQuickAccess(string addFolderPath) // 新增快速存取
+        static void mNewLine()
         {
-            try
-            {
-                Type shellAppType = Type.GetTypeFromProgID("Shell.Application");
-                Object shell = Activator.CreateInstance(shellAppType);
-                Shell32.Folder2 f2 = (Shell32.Folder2)shellAppType.InvokeMember
-                    ("NameSpace", System.Reflection.BindingFlags.InvokeMethod, null, shell, new object[] { addFolderPath });
-                f2.Self.InvokeVerb("pintohome");
-                Console.WriteLine("新增單筆成功：" + addFolderPath);
-            }
-            catch (Exception e) { Console.WriteLine("新增單筆失敗：" + addFolderPath + " 錯誤碼：" + e.Message); }
+            Console.WriteLine();
         }
-
-        static void delQuickAccess() // 移除快速存取
+        
+        static void mCancel()
         {
-            string delFolderName = String.Empty;
-
-            try
-            {
-                bool run = true;
-
-                do
-                {
-                    Type shellAppType = Type.GetTypeFromProgID("Shell.Application");
-                    Object shell = Activator.CreateInstance(shellAppType);
-                    Shell32.Folder2 f2 = (Shell32.Folder2)shellAppType.InvokeMember("NameSpace", System.Reflection.BindingFlags.InvokeMethod, null, shell, new object[] { "shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}" });
-                    int count = 1;
-                    Dictionary<int, string> dCurrentList = new Dictionary<int, string>(); // 存放目前的快速存存清單
-
-                    foreach (Shell32.FolderItem fi in f2.Items()) // 列出清單、加入 Dictionary
-                    {
-                        Console.WriteLine(count + "：" + fi.Name);
-                        dCurrentList.Add(count, fi.Name);
-                        count++;
-                    }
-                    Console.WriteLine();
-                    Console.Write("請選擇要移除的項目，輸入 0 離開：");
-
-                    bool success = Int32.TryParse(Console.ReadLine(), out int number); // 轉換選項
-                    Console.WriteLine();
-                    if (number == 0) // 離開
-                    {
-                        run = false;
-                        return;
-                    }
-                    if (dCurrentList.ContainsKey(number)) // 用 key 尋找 Dictionary，找到的話 delFolderName 賦值。
-                        delFolderName = dCurrentList[number];
-                    else
-                    {
-                        Console.WriteLine("找不到項目：" + number);
-                    }
-
-                    if (success)
-                    {
-                        foreach (Shell32.FolderItem fi in f2.Items())
-                        {
-                            if (fi.Name == delFolderName)
-                            {
-                                ((Shell32.FolderItem)fi).InvokeVerb("unpinfromhome");
-                                Console.WriteLine("刪除單筆完成：" + delFolderName);
-                            }
-                        }
-                    }
-                    separator();
-                } while (run);
-            }
-            catch (Exception e) { Console.WriteLine("刪除單筆失敗：" + delFolderName + " 錯誤碼：" + e.Message); }
+            Console.WriteLine("已取消");
         }
     }
 }
